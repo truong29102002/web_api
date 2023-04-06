bc0b42a027d8625ac8e69c1576bcaf717cc55382
TDT <xuantruong0944@gmail.com>
2023-04-07 00:15:36 +0700

    upadate

---
+++ web_api/web_api/Controllers/SinhVienController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using web_api.Models;
+
+namespace web_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SinhVienController : ControllerBase
+    {
+        public static List<SinhVien> sv = new List<SinhVien>();
+        [HttpGet(Name = "GetAll")]
+        public IActionResult GetAll()
+        {
+            return Ok(sv);
+        }
+        [HttpPost]
+        public IActionResult CreateSv(SinhVien svn)
+        {
+            SinhVien svm = new SinhVien
+            {
+                id= Guid.NewGuid(), Name = svn.Name
+            };
+            sv.Add(svm);
+            return Ok(new
+            {
+                Success = true,
+                Data = sv
+            });
+        }
+    }
+}
---
+++ web_api/web_api/Models/SinhVien.cs
@@ -0,0 +1,9 @@
+﻿namespace web_api.Models
+{
+    public class SinhVien
+    {
+        public Guid id { get; set; }
+        public string? Name { get; set; }
+    }
+
+}
