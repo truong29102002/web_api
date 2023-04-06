b831a564e4f5b64e6327b1545cdfaee05c011d98
TDT <xuantruong0944@gmail.com>
2023-04-07 00:51:49 +0700

    commit

--- web_api/web_api/Controllers/SinhVienController.cs
+++ web_api/web_api/Controllers/SinhVienController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using web_api.Models;
 
 namespace web_api.Controllers
@@ -14,12 +15,33 @@ namespace web_api.Controllers
         {
             return Ok(sv);
         }
+        [HttpGet("{id}")] // http:localhost:xxx//api/sinhvien/{id}
+        public IActionResult Get(Guid id)
+        {
+            try
+            {
+                var svt = sv.SingleOrDefault(x => x.id == id);
+                if (svt == null)
+                {
+                    return NotFound();
+                }
+                return Ok(svt);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message); 
+            }
+           
+        }
+
         [HttpPost]
         public IActionResult CreateSv(SinhVien svn)
         {
             SinhVien svm = new SinhVien
             {
-                id= Guid.NewGuid(), Name = svn.Name
+                id = Guid.NewGuid(),
+                Name = svn.Name
             };
             sv.Add(svm);
             return Ok(new
@@ -28,5 +50,53 @@ namespace web_api.Controllers
                 Data = sv
             });
         }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateSv(Guid id,  SinhVien svn)
+        {
+            try
+            {
+                var svt = sv.SingleOrDefault(x => x.id == id);
+                if (svt == null)
+                {
+                    return NotFound();
+                }
+                if(svt.id != svn.id)
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    svt.id = svn.id;
+                    svt.Name = svn.Name;
+                }
+                return Ok(svt);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteSv(Guid id)
+        {
+            try
+            {
+                var svt = sv.SingleOrDefault(x => x.id == id);
+                if (svt == null)
+                {
+                    return NotFound();
+                }
+                sv.Remove(svt);
+                return Ok(svt);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
     }
 }
